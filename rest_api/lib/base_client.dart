bd6626c69e90c19ade5220b05fcb397e6dc53473
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-11 09:47:41 -0300

    fixing bugs

--- rest_api/lib/base_client.dart
+++ rest_api/lib/base_client.dart
@@ -5,8 +5,14 @@ const String baseUrl = 'https://631c37911b470e0e12fcdd0b.mockapi.io/api';
 class BaseClient {
   var client = http.Client()
   Future<dynamic> get(String api) async {
-    var url = Uri.parse(api);
-    client.get(url);
+    var url = Uri.parse(baseUrl + api);
+
+    var response = await client.get(url);
+    if (responde.statusCode == 200) {
+      return response.body;
+    } else {
+      //throw exception and catch it in UI
+    }
   }
 
   Future<dynamic> post(String api) async {
