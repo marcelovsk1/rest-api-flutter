9eeada1fc1c64a878f1df21a5c5c9a264ab680c0
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-11 11:45:09 -0300

    url uri

--- rest_api/lib/home.dart
+++ rest_api/lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +11,20 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Rest API Call')
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: fetchEvents,
+      ),
+    );
+  }
+
+  void fetchEvents() {
+    print('fetchEvents called');
+    const url = '';
+    final uri = Uri.parse(url);
+    http.get(uri);
   }
 }
