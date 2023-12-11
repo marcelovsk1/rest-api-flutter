714a9a01d5a9b174c44788524f2a1111e2e7810c
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-11 09:45:04 -0300

    working on

--- rest_api/lib/base_client.dart
+++ rest_api/lib/base_client.dart
@@ -1,6 +1,21 @@
+import 'package:http/http.dart' as http;
+
+const String baseUrl = 'https://631c37911b470e0e12fcdd0b.mockapi.io/api';
+
 class BaseClient {
-  Future<dynamic> get(String api) async {}
-  Future<dynamic> post(String api) async {}
-  Future<dynamic> put(String api) async {}
-  Future<dynamic> delete(String api) async {}
+  var client = http.Client()
+  Future<dynamic> get(String api) async {
+    var url = Uri.parse(api);
+    client.get(url);
+  }
+
+  Future<dynamic> post(String api) async {
+
+  }
+  Future<dynamic> put(String api) async {
+
+  }
+  Future<dynamic> delete(String api) async {
+
+  }
 }
