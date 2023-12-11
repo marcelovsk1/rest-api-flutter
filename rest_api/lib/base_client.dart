6fa4d2f4773532546ec4464ae642c60ac0dbaacf
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-11 10:28:51 -0300

    working on

--- rest_api/lib/base_client.dart
+++ rest_api/lib/base_client.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:rest_api/user.dart';
 
@@ -8,23 +9,35 @@ class BaseClient {
 
   // GET
   Future<dynamic> get(String api) async {
-    var url = Uri.parse(baseUrl + api);
-    var _headers = {
-      'Authorization': 'Bearer sfie328370428387=',
-      'api_key': 'ief873fj38uf38uf83u839898989',
-    };
+  var url = Uri.parse(baseUrl + api);
+  var _headers = {
+    'Authorization': 'Bearer sfie328370428387=',
+    'api_key': 'ief873fj38uf38uf83u839898989',
+  };
 
-    var response = await client.get(url, headers: _headers);
-    if (response.statusCode == 200) {
-      return response.body;
-    } else {
-      //throw exception and catch it in UI
-    }
+  var response = await client.get(url, headers: _headers);
+  if (response.statusCode == 200) {
+    return json.decode(response.body);
+  } else {
+    // throw exception and catch it in UI
   }
+}
 
-  Future<dynamic> post(String api, User user) async {
+  Future<dynamic> post(String api, dynamic object) async {
+  var url = Uri.parse(baseUrl + api);
+  var _payload = json.encode(object);
+  var _headers = {
+    'Authorization': 'Bearer sfie328370428387=',
+    'api_key': 'ief873fj38uf38uf83u839898989',
+  };
 
+  var response = await client.post(url, body: _payload, headers: _headers);
+  if (response.statusCode == 201) {
+    return response.body;
+  } else {
+    // throw exception and catch it in UI
   }
+}
   Future<dynamic> put(String api, User user) async {
 
   }
