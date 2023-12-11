757f59bcb015ccec4c8c755c86c6376926a02575
Marcelo Amaral Alves <marceloaa@live.com>
2023-12-11 09:26:17 -0300

    working on

--- rest_api/lib/base_client.dart
+++ rest_api/lib/base_client.dart
@@ -1,3 +1,6 @@
 class BaseClient {
-
+  Future<dynamic> get(String api) async {}
+  Future<dynamic> post(String api) async {}
+  Future<dynamic> put(String api) async {}
+  Future<dynamic> delete(String api) async {}
 }
